553ee70e6930ba17404036aa172e0585339e33db
xb0yx2k20 <musraul@yandex.ru>
2022-08-02 13:56:23 +0300

    New website

---
+++ website/script.js
@@ -0,0 +1,60 @@
+"use strict"
+
+
+// Определение устройства
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    IOS: function () {
+        return navigator.userAgent.match(/Iphone|Ipad|Ipod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.IOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+} else {
+    document.body.classList.add('_pc');
+}
+
+
+// Плавная прокрутка
+
+const menulinks = document.querySelectorAll('.menu_link[data-goto]')
+if (menulinks.length > 0) {
+    menulinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
