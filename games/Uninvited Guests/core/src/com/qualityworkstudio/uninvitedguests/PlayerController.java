43daa1e4007224602805ba1bf41d6a9d848f88db
Andrey-Karanik <qws.main@gmail.com>
2022-08-02 17:42:48 +0300

    Add PlayerController

---
+++ games/Uninvited Guests/core/src/com/qualityworkstudio/uninvitedguests/PlayerController.java
@@ -0,0 +1,13 @@
+package com.qualityworkstudio.uninvitedguests;
+
+/**
+ *
+ * @author Andrey Karanik
+ * @see BasicPlayerController
+ */
+
+public interface PlayerController {
+    public void move();
+
+    public void look();
+}
