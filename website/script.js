0d77d9bc1be4a15ad97421eb27cc4b685a108343
xb0yx2k20 <musraul@yandex.ru>
2022-08-06 15:47:22 +0300

    Added Menu Burger

--- website/script.js
+++ website/script.js
@@ -50,6 +50,11 @@ if (menulinks.length > 0) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
             window.scrollTo({
                 top: gotoBlockValue,
                 behavior: "smooth"
@@ -57,4 +62,16 @@ if (menulinks.length > 0) {
             e.preventDefault();
         }
     }
+}
+
+// Меню бургер
+
+const iconMenu = document.querySelector('.menu_icon');
+const menuBody = document.querySelector('.menu_body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
 }
