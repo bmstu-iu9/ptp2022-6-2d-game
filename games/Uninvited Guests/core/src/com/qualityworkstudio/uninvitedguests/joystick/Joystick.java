53fdacec5ca1ae675391b6703d19422746531290
BogdanTeryukhov <andrejteryukhov5@gmail.com>
2022-08-16 19:45:12 +0300

    added interface Joystick

---
+++ games/Uninvited Guests/core/src/com/qualityworkstudio/uninvitedguests/joystick/Joystick.java
@@ -0,0 +1,16 @@
+package com.qualityworkstudio.uninvitedguests.joystick;
+
+/**
+ * @author Bogdan Teryukhov
+ */
+import com.badlogic.gdx.math.Vector2;
+
+public interface Joystick {
+    void show();
+    void hide();
+    Vector2 getDirection();
+    double getAngle();
+    void setPosition(Vector2 position);
+    Vector2 getPosition();
+    void setRadius(float radius);
+}
