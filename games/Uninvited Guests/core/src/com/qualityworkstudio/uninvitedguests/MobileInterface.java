3933a97be3a531e8032e56eb78c7dd32d361e6ae
Andrey-Karanik <qws.main@gmail.com>
2022-08-20 16:45:43 +0300

    Add MobileInterface

---
+++ games/Uninvited Guests/core/src/com/qualityworkstudio/uninvitedguests/MobileInterface.java
@@ -0,0 +1,46 @@
+package com.qualityworkstudio.uninvitedguests;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.qualityworkstudio.uninvitedguests.joystick.BasicJoystick;
+import com.qualityworkstudio.uninvitedguests.joystick.Joystick;
+
+/**
+ * @author Andrey Karanik
+ */
+
+public class MobileInterface {
+
+    private Joystick movementJoystick;
+    private Joystick rotationJoystick;
+
+    public MobileInterface(Stage stage, AssetManager assetManager) {
+        movementJoystick = new BasicJoystick(stage, assetManager);
+        rotationJoystick = new BasicJoystick(stage, assetManager);
+        movementJoystick.setSize(new Vector2(420, 420));
+        rotationJoystick.setSize(new Vector2(420, 420));
+        movementJoystick.setRadius(160);
+        rotationJoystick.setRadius(160);
+        movementJoystick.setPosition(new Vector2(235, 235));
+        rotationJoystick.setPosition(new Vector2(stage.getWidth() - 235, 235));
+    }
+
+    public void show() {
+        movementJoystick.show();
+        rotationJoystick.show();
+    }
+
+    public void hide() {
+        movementJoystick.hide();
+        rotationJoystick.hide();
+    }
+
+    public Joystick getMovementJoystick() {
+        return movementJoystick;
+    }
+
+    public Joystick getRotationJoystick() {
+        return rotationJoystick;
+    }
+}
