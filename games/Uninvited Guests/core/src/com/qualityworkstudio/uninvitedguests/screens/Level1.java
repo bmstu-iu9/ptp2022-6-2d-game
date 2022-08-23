2f7855df7ec38f86090d42aa2e4fdcef5d4f4c00
Andrey-Karanik <qws.main@gmail.com>
2022-08-23 14:07:06 +0300

    Add Level1

---
+++ games/Uninvited Guests/core/src/com/qualityworkstudio/uninvitedguests/screens/Level1.java
@@ -0,0 +1,58 @@
+package com.qualityworkstudio.uninvitedguests.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.qualityworkstudio.uninvitedguests.BasicDoor;
+import com.qualityworkstudio.uninvitedguests.GameSettings;
+import com.qualityworkstudio.uninvitedguests.Map;
+
+import java.util.ArrayList;
+
+public class Level1 extends LevelScreen {
+
+    private Map map;
+    private ArrayList<BasicDoor> doors;
+
+    public Level1(Game game, AssetManager assetManager, GameSettings gameSettings) {
+        super(game, assetManager, gameSettings);
+        map = new Map(128, assetManager.<Texture>get("maps/lvl1_map_layer1.png"), assetManager.<Texture>get("maps/lvl1_map_layer2.png"));
+        doors = new ArrayList<>();
+        BasicDoor door1 = new BasicDoor(world, assetManager);
+        door1.setType(BasicDoor.Type.RED);
+        door1.setPosition(0f, -56f);
+        doors.add(door1);
+        BasicDoor door2 = new BasicDoor(world, assetManager);
+        door2.setType(BasicDoor.Type.GREEN);
+        door2.setPosition(0f, -16f);
+        doors.add(door2);
+        BasicDoor door3 = new BasicDoor(world, assetManager);
+        door3.setType(BasicDoor.Type.YELLOW);
+        door3.setPosition(0f, 32f);
+        doors.add(door3);
+        player.setPosition(0f, -36f);
+    }
+
+    @Override
+    public void update(float delta) {
+        player.update(delta);
+        for (BasicDoor door : doors) {
+            door.update(delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            game.setScreen(new MainScreen(game, assetManager, settings));
+        }
+    }
+
+    @Override
+    public void draw() {
+        map.draw(batch);
+        for (BasicDoor door : doors) {
+            door.draw(batch);
+        }
+        map.draw(batch);
+        player.draw(batch);
+    }
+}
