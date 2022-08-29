b5c9f40628d85c679f3af4d7ef49ce0733e80771
Andrey-Karanik <qws.main@gmail.com>
2022-08-29 19:03:26 +0300

    Add Box

---
+++ games/Uninvited Guests/core/src/com/qualityworkstudio/uninvitedguests/Box.java
@@ -0,0 +1,55 @@
+package com.qualityworkstudio.uninvitedguests;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Filter;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Box {
+
+    private Body body;
+    private Sprite sprite;
+
+    public Box(World world, Texture texture, Vector2 spriteSize, Vector2 bodySize) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.linearDamping = 15;
+        bodyDef.angularDamping = 15;
+        body = world.createBody(bodyDef);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(bodySize.x, bodySize.y);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.5f;
+        Fixture fixture = body.createFixture(fixtureDef);
+        Filter filter = new Filter();
+        filter.groupIndex = GroupIndices.BOX;
+        fixture.setFilterData(filter);
+        shape.dispose();
+
+        sprite = new Sprite(texture);
+        sprite.setSize(spriteSize.x, spriteSize.y);
+        sprite.setOriginCenter();
+    }
+
+    public void update() {
+        sprite.setPosition(body.getPosition().x - sprite.getWidth() / 2f, body.getPosition().y - sprite.getHeight() / 2f);
+        sprite.setRotation((float)Math.toDegrees(body.getAngle()));
+    }
+
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+
+    public void setPosition(float x, float y) {
+        body.setTransform(x, y, body.getAngle());
+    }
+}
