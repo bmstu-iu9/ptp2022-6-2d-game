fc04c2400c773d8641751125be3470b3a01999ae
xb0yx2k20 <musraul@yandex.ru>
2022-09-15 22:09:54 +0300

    Updated website

--- website/script.js
+++ website/script.js
@@ -10,7 +10,7 @@ if (menulinks.length > 0) {
         const menuLink = e.target;
         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
 
             if (iconMenu.classList.contains('_active')) {
                 document.body.classList.remove('_lock');
